6fbc8dc16378a1e5c09cb4289edad930a99c0356
sombartu <petya007s@gmail.com>
2024-04-25 01:53:00 +0300

    new

---
+++ lesson__one/index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Practice</title>
+</head>
+<body>
+<header class="header">
+    <div class="container">
+<!--        <h1>Hello</h1>-->
+    </div>
+</header>
+</body>
+</html>
+
+<?php
+   echo '<b>Первый урок:</b> Как сделать коментарии и написать первые слова на старнице!';
+   echo '<br>';
+   /*
+    echo '<b>Hello</b> World!';
+    */
+//   echo '<br>';
+#   echo 'Hello World!';
+
+?>
