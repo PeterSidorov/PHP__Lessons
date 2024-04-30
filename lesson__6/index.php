cc84e3365b720ac079c5c4645146861cf1a59b79
sombartu <petya007s@gmail.com>
2024-04-30 15:07:59 +0300

    g

---
+++ lesson__6/index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="background-color: lightcoral">
+<?php
+
+$x = 5;
+
+switch ($x) {
+    case 1:
+        echo "x равно 0";
+        break;
+    case 2:
+        echo "x равно 1";
+        break;
+    case 3:
+        echo "x равно 2";
+        break;
+
+    default:
+        echo "x non";
+}
+
+?>
+</body>
+</html>
+<?php
